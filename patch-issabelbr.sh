690066bc6f3084d77537f6fabb18ca4734b1cf9b
ibinetwork <rafael@ibinetwork.com.br>
2018-04-23 17:28:36 -0300

    Add files via upload

---
+++ patch-issabelbr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo " _____               _          _    _    ____  _____  "
+echo '|_   _|             | |        | |/\| |/\|  _ \|  __ \ '
+echo '  | |  ___ ___  __ _| |__   ___| |\ ` ´ /| |_) | |__) |'
+echo "  | | / __/ __|/ _\` | '_ \ / _ \ |_     _|  _ <|  _  /"
+echo ' _| |_\__ \__ \ (_| | |_) |  __/ |/ , . \| |_) | | \ \ '
+echo '|_____|___/___/\__,_|_.__/ \___|_|\/|_|\/|____/|_|  \_\'
+echo "======================================================="
+echo "Patch Brasileiro para Issabel"
+echo "Grupo Telegram http://t.me/issabelbr"
+echo ""
+echo "Atualizando o sistema..."
+echo ""
+yum update -y                                                    
