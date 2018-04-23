f00be2f9228481d64cf85f2440455a71a2471c7d
ibinetwork <rafael@ibinetwork.com.br>
2018-04-23 18:10:02 -0300

    Update index.php

--- modules/cdrreport/index.php
+++ modules/cdrreport/index.php
@@ -293,6 +293,21 @@ function _moduleContent(&$smarty, $module_name)
                 $arrTmp[4] = $value[3];
                 $arrTmp[5] = $value[9];
                 $arrTmp[6] = $value[4];
+                    if ($value[5] == "ANSWERED") {
+				            $value[5] = "<font color=green>ATENDIDO</font>";
+			              }
+			              elseif ($value[5] == "NO ANSWER") {
+                    $value[5] = "<font color=red>NÃO ATENDIDO</font>";
+			              }
+                    elseif ($value[5] == "BUSY") {
+                    $value[5] = "<font color=ambar>OCUPADO</font>";
+                    }
+                    elseif ($value[5] == "FAILED") {
+                    $value[5] = "<font color=red>FALHOU</font>";
+                    }
+			              else {
+				            $value[5] = "<font color=red>$value[5]</font>";
+			              }
                 $arrTmp[7] = $value[5];
                 $iDuracion = $value[8];
                 $iSec = $iDuracion % 60; $iDuracion = (int)(($iDuracion - $iSec) / 60);
