b4f4578420cdd2c2fee4b54a36cb39f95a524271
Rafael Tavares <rafael@ibinetwork.com.br>
2018-10-10 12:19:52 -0300

    Update patch-issabelbr.sh

--- patch-issabelbr.sh
+++ patch-issabelbr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-versao="1.1.2"
+versao="1.1.3"
 clear
 echo " _____               _          _    _    ____  _____  "
 echo "|_   _|             | |v$versao "' | |/\| |/\|  _ \|  __ \ '
