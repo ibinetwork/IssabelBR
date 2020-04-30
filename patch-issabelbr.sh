c35276af4ab4e2fc5fe6795f3052d0a001b63523
Rafael Tavares <rafael@ibinetwork.com.br>
2020-04-30 12:13:48 -0300

    Update 30/04/2020

--- patch-issabelbr.sh
+++ patch-issabelbr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-versao="1.1.7"
+versao="1.2.0"
 clear
 echo " _____               _          _    _    ____  _____  "
 echo "|_   _|             | |v$versao "' | |/\| |/\|  _ \|  __ \ '
@@ -32,11 +32,11 @@ echo "Instalando patch de idiomas, cdr e bilhetagem..."
 echo ""
 svn co https://github.com/ibinetwork/IssabelBR/trunk/ /usr/src/IssabelBR
 cp /var/www/html/modules/billing_report/index.php /var/www/html/modules/billing_report/index.php.bkp
-cp /var/www/html/modules/cdrreport/index.php /var/www/html/modules/cdrreport/index.php.bkp
+#cp /var/www/html/modules/cdrreport/index.php /var/www/html/modules/cdrreport/index.php.bkp
 cp /var/www/html/modules/monitoring/index.php /var/www/html/modules/monitoring/index.php.bkp
 cp /var/www/html/modules/campaign_monitoring/index.php /var/www/html/modules/campaign_monitoring/index.php.bkp
 rsync --progress -r /usr/src/IssabelBR/web/ /var/www/html/
-amportal restart
+#amportal restart
 echo ""
 echo "Instalando audio em Português Brasil"
 echo ""
@@ -48,8 +48,8 @@ echo "language=pt_BR" >> /etc/asterisk/iax_general_custom.conf
 sed -i '/defaultlanguage=pt_BR/d' /etc/asterisk/asterisk.conf
 echo "defaultlanguage=pt_BR" >> /etc/asterisk/asterisk.conf
 echo ""
-echo "Instalando codec g729"
-echo ""
+#echo "Instalando codec g729"
+#echo ""
 echo "Instalando tratamento Hangup Cause"
 echo ""
 sed -i '/extensions_tratamento_hangupcause.conf/d' /etc/asterisk/extensions_override_issabel.conf
@@ -61,45 +61,38 @@ chown asterisk.asterisk /etc/asterisk/extensions_tratamento_hangupcause.conf
 echo ""
 chown -R asterisk.asterisk /var/lib/asterisk/agi-bin/*
 chown -R asterisk.asterisk /var/lib/asterisk/agi-bin/
-test=`asterisk -V | grep "13"`
-if [[ -z $test ]]; then
- release="11"
-else
- release="13"
-fi
-if [[ "$release" = "13" ]]; then
- cp /usr/src/IssabelBR/codecs/codec_g729-ast130-gcc4-glibc2.2-x86_64-pentium4.so /usr/lib64/asterisk/modules/codec_g729.so
- chmod 755 /usr/lib64/asterisk/modules/codec_g729.so
- asterisk -rx "module load codec_g729"
- rsync --progress -r -u /usr/src/IssabelBR/callcenter13/ /opt/issabel/dialer/
- chown asterisk.asterisk /opt/issabel/dialer/
- echo ""
- echo "Ajustando arquivo features.conf para Asterisk 13"
- echo ""
- cp /var/www/html/admin/modules/parking/functions.inc/dialplan.php /var/www/html/admin/modules/parking/functions.inc/dialplan.php.bkp
- CHECKFILE=$(sed '63!d' /var/www/html/admin/modules/parking/functions.inc/dialplan.php); if [[ "${CHECKFILE}" == *"addFeatureGeneral('parkedplay"* ]]; then sed -i '63d' /var/www/html/admin/modules/parking/functions.inc/dialplan.php; echo "Ajuste efetuado"; else echo "Não é necessário efetuar o ajuste"; fi
- sed -i '/parkedplay=both/d' /etc/asterisk/features_general_additional.conf
- echo ""
- yum install asterisk13-sqlite3.x86_64 -y
- mv -n /etc/asterisk/cdr_sqlite3_custom.conf /etc/asterisk/cdr_sqlite3_custom.conf.bkp
- mv -n /etc/asterisk/cdr_sqlite3_custom_a13.conf /etc/asterisk/cdr_sqlite3_custom.conf
- sed -i '/app_mysql.so/d' /etc/asterisk/modules_custom.conf
- echo "noload => appmysql.so" >> /etc/asterisk/modules_custom.conf
- sed -i '/cdr_mysql.so/d' /etc/asterisk/modules_custom.conf
- echo "noload => cdrmysql.so" >> /etc/asterisk/modules_custom.conf
-else
- cp /usr/src/IssabelBR/codecs/codec_g729-ast110-gcc4-glibc-x86_64-pentium4.so /usr/lib64/asterisk/modules/codec_g729.so
- chmod 755 /usr/lib64/asterisk/modules/codec_g729.so
- asterisk -rx "module load codec_g729"
-fi
+#test=`asterisk -V | grep "13"`
+#if [[ -z $test ]]; then
+# release="11"
+#else
+# release="13"
+#fi
+#if [[ "$release" = "13" ]]; then
+# cp /usr/src/IssabelBR/codecs/codec_g729-ast130-gcc4-glibc2.2-x86_64-pentium4.so /usr/lib64/asterisk/modules/codec_g729.so
+# chmod 755 /usr/lib64/asterisk/modules/codec_g729.so
+# asterisk -rx "module load codec_g729"
+# rsync --progress -r -u /usr/src/IssabelBR/callcenter13/ /opt/issabel/dialer/
+# chown asterisk.asterisk /opt/issabel/dialer/
+# echo ""
+# echo "Ajustando arquivo features.conf para Asterisk 13"
+# echo ""
+# cp /var/www/html/admin/modules/parking/functions.inc/dialplan.php /var/www/html/admin/modules/parking/functions.inc/dialplan.php.bkp
+# CHECKFILE=$(sed '63!d' /var/www/html/admin/modules/parking/functions.inc/dialplan.php); if [[ "${CHECKFILE}" == *"addFeatureGeneral('parkedplay"* ]]; then sed -i '63d' /var/www/html/admin/modules/parking/functions.inc/dialplan.php; echo "Ajuste efetuado"; else echo "Não é necessário efetuar o ajuste"; fi
+# sed -i '/parkedplay=both/d' /etc/asterisk/features_general_additional.conf
+# echo ""
+# yum install asterisk13-sqlite3.x86_64 -y
+# mv -n /etc/asterisk/cdr_sqlite3_custom.conf /etc/asterisk/cdr_sqlite3_custom.conf.bkp
+# mv -n /etc/asterisk/cdr_sqlite3_custom_a13.conf /etc/asterisk/cdr_sqlite3_custom.conf
+# sed -i '/app_mysql.so/d' /etc/asterisk/modules_custom.conf
+# echo "noload => appmysql.so" >> /etc/asterisk/modules_custom.conf
+# sed -i '/cdr_mysql.so/d' /etc/asterisk/modules_custom.conf
+# echo "noload => cdrmysql.so" >> /etc/asterisk/modules_custom.conf
+#else
+# cp /usr/src/IssabelBR/codecs/codec_g729-ast110-gcc4-glibc-x86_64-pentium4.so /usr/lib64/asterisk/modules/codec_g729.so
+# chmod 755 /usr/lib64/asterisk/modules/codec_g729.so
+# asterisk -rx "module load codec_g729"
+#fi
 echo ""
-#echo "Recompilando DAHDI"
-#echo ""
-#cd /usr/src/dahdi-linux-2.11.1/
-#make
-#make install
-#dahdi_tools
-#echo ""
 echo "Instalando sngrep"
 echo "" 
 rm -Rf /etc/yum.repos.d/irontec.repo
@@ -111,15 +104,15 @@ EOF
 rpm --import http://packages.irontec.com/public.key
 yum install sngrep -y
 echo ""
-echo "REMOVENDO CADASTRO TELA INDEX"
+#echo "REMOVENDO CADASTRO TELA INDEX"
 echo ""
 sed -i -r 's/666699/33697B/' /var/www/html/modules/pbxadmin/themes/default/css/mainstyle.css
 sed -i -r 's/666699/33697B/' /var/www/html/admin/assets/css/mainstyle.css
-sed -i '/neo-modal-issabel-popup-box/d' /var/www/html/themes/tenant/_common/index.tpl
-sed -i '/neo-modal-issabel-popup-title/d' /var/www/html/themes/tenant/_common/index.tpl
-sed -i '/neo-modal-issabel-popup-close/d' /var/www/html/themes/tenant/_common/index.tpl
-sed -i '/neo-modal-issabel-popup-content/d' /var/www/html/themes/tenant/_common/index.tpl
-sed -i '/neo-modal-issabel-popup-blockmask/d' /var/www/html/themes/tenant/_common/index.tpl
+#sed -i '/neo-modal-issabel-popup-box/d' /var/www/html/themes/tenant/_common/index.tpl
+#sed -i '/neo-modal-issabel-popup-title/d' /var/www/html/themes/tenant/_common/index.tpl
+#sed -i '/neo-modal-issabel-popup-close/d' /var/www/html/themes/tenant/_common/index.tpl
+#sed -i '/neo-modal-issabel-popup-content/d' /var/www/html/themes/tenant/_common/index.tpl
+#sed -i '/neo-modal-issabel-popup-blockmask/d' /var/www/html/themes/tenant/_common/index.tpl
 echo ""
 echo "ALTERANDO MUSICONHOLD AGENTS"
 echo ""
